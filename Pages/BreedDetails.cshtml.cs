1e217d636b24ef032876ced0b9ea5d9911060cf3
Amelia-Pencak <amelia.pencak@gmail.com>
2024-05-25 21:37:51 +0200

    layout v1

---
+++ Pages/BreedDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CatBreedCatalog.Pages
+{
+    public class BreedDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ wwwroot/js/pages/createPage/displayImage.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var imageInput = document.getElementById('image');
+    if (imageInput) {
+        imageInput.addEventListener('change', function (event) {
+            var output = document.getElementById('preview-image');
+            output.src = URL.createObjectURL(event.target.files[0]);
+            output.onload = function () {
+                URL.revokeObjectURL(output.src) // Oczyszcza pamięci po załadowaniu zdjecia
+            };
+            output.style.display = 'block'; // Pokazuje zdjecie, gdy plik zostanie wybrany
+        });
+    }
+});

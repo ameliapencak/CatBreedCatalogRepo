da98ff47a6f2178cbb9438942695fca837ce786b
Amelia-Pencak <amelia.pencak@gmail.com>
2024-05-30 20:36:46 +0200

    Layout ver-2

---
+++ Pages/EditBreed.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CatBreedCatalog.Pages
+{
+    public class EditBreedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

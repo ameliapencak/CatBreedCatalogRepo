94b4d456580fce0299b8841c2313d4e844e25b46
Amelia-Pencak <amelia.pencak@gmail.com>
2024-05-31 22:48:02 +0200

    creating new record

--- Pages/Create.cshtml.cs
+++ Pages/Create.cshtml.cs
@@ -1,3 +1,5 @@
+using CatBreedCatalog.Data;
+using CatBreedCatalog.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,52 @@ namespace CatBreedCatalog.Pages
 {
     public class CreateModel : PageModel
     {
+        private readonly CatBreedContext _context;
+
+        public CreateModel(CatBreedContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public CatBreed CatBreed { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var newCatBreed = new CatBreed
+            {
+                Name = CatBreed.Name,
+                Description = CatBreed.Description,
+                Origin = CatBreed.Origin,
+                Temperament = CatBreed.Temperament
+            };
+
+            if (Request.Form.Files.Count > 0)
+            {
+                var file = Request.Form.Files[0];
+                if (file.Length > 0)
+                {
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        await file.CopyToAsync(memoryStream);
+                        newCatBreed.ImageData = memoryStream.ToArray();
+                    }
+                }
+            }
+
+            _context.CatBreeds.Add(newCatBreed);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("/BreedDetails", new { id = newCatBreed.Id });
         }
     }
 }
